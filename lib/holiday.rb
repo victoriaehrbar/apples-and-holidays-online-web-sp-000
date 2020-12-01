09dcca7a63ef0fae066cffd0c34985879c76a7a5
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-01 02:22:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,6 +35,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash.map do |season, holiday|
+holiday_hash.map do |season, holidays|
+  holidays.map do |holiday_name, supplies|
 end
 end
